3b0e304cffdaf5a7d5c5163094fe93f06ef410dc
nohell <noel.hmnn@gmail.com>
2021-12-13 17:57:27 +0100

    switching between background color and 2 new lines

--- index.js
+++ index.js
@@ -2,17 +2,34 @@ const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        datasets: [{
+        datasets: [
+        {   //red graph
             label: 'ping to cloudflare',
             pointRadius: 0,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(99, 255, 132, 0.2)',
-                'rgba(130, 160, 255, 0.2)'
+                'rgba(255, 99, 132, 0.0)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 0.8)',
-                'rgba(99, 255, 132, 0.8)',
+                'rgba(255, 99, 132, 0.8)'
+            ],
+            borderWidth: 1
+        },{ //green graph
+            label: 'ping to google',
+            pointRadius: 0,
+            backgroundColor: [               
+                'rgba(99, 255, 132, 0.0)'
+            ],
+            borderColor: [
+                'rgba(99, 255, 132, 0.8)'
+            ],
+            borderWidth: 1
+        },{//blue graph
+            label: 'ping to valorant',
+            pointRadius: 0,
+            backgroundColor: [               
+                'rgba(130, 160, 255, 0.0)'
+            ],
+            borderColor: [
                 'rgba(130, 160, 255, 0.8)'
             ],
             borderWidth: 1
@@ -25,7 +42,7 @@ const myChart = new Chart(ctx, {
                     color: "rgba(53,54,58,1)",
                 },
                 ticks: {
-                    display: false //this will remove only the label
+                    display: false
                 }
             }],
             yAxes: [{
@@ -40,28 +57,58 @@ const myChart = new Chart(ctx, {
     }
 });
 
+function rnd(num){
+    return Math.floor(Math.random() * num + 1);
+}
 
-let started = false;
-function checkArray(item){
-    if(item.length > 20)
-    {
-        item.shift();
-        started = true;
+document.getElementById("myChart").addEventListener("click", clickChart);
+
+let counter = 0;
+function clickChart(){
+    counter ++;
+    if(counter == 1){
+        myChart.data.datasets[0].backgroundColor = 'rgba(255, 99, 132, 0.2)';
+        myChart.data.datasets[1].backgroundColor = 'rgba(99, 255, 132, 0.0)';
+        myChart.data.datasets[2].backgroundColor = 'rgba(130, 160, 255, 0.0)';
+    }
+    else if(counter == 2){
+        myChart.data.datasets[0].backgroundColor = 'rgba(255, 99, 132, 0.0)';
+        myChart.data.datasets[1].backgroundColor = 'rgba(99, 255, 132, 0.2)';
+        myChart.data.datasets[2].backgroundColor = 'rgba(130, 160, 255, 0.0)';
+    }
+    else if(counter == 3){
+        myChart.data.datasets[0].backgroundColor = 'rgba(255, 99, 132, 0.0)';
+        myChart.data.datasets[1].backgroundColor = 'rgba(99, 255, 132, 0.0)';
+        myChart.data.datasets[2].backgroundColor = 'rgba(130, 160, 255, 0.2)';
+    }
+    else if(counter == 4){
+        myChart.data.datasets[0].backgroundColor = 'rgba(255, 99, 132, 0.0)';
+        myChart.data.datasets[1].backgroundColor = 'rgba(99, 255, 132, 0.0)';
+        myChart.data.datasets[2].backgroundColor = 'rgba(130, 160, 255, 0.0)';
+    }
+    else{
+        counter = 0;
+        clickChart();
     }
 }
 
-myChart.data.datasets[0].data = [0,0,0,0,0,2,2,10,2,2,0,0,0,5,5,5,5,5,5,5]
-myChart.data.labels = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+function init(){
+    myChart.data.datasets[0].data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    myChart.data.datasets[1].data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    myChart.data.datasets[2].data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    myChart.data.labels = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+}
 
 function update_graph(graph){
-    if(Math.random()*10 > 9.5){
-        graph.data.datasets[0].data.push(Math.random()*10*5);
-    }
-    else{
-        graph.data.datasets[0].data.push(Math.random()*10);
-    }
-    checkArray(graph.data.datasets[0].data)
-    if(started == false) graph.data.labels.push(0);    
+
+    graph.data.datasets[0].data.push(rnd(100));
+    graph.data.datasets[1].data.push(rnd(100));
+    graph.data.datasets[2].data.push(rnd(100));
+
+    graph.data.datasets[0].data.shift();
+    graph.data.datasets[1].data.shift();
+    graph.data.datasets[2].data.shift();
+ 
     graph.update();
 }
 
@@ -69,9 +116,10 @@ function interval(){
     update_graph(myChart);
 }
 
+init();
 var intervalId = window.setInterval(function(){
     interval();
-}, 1000);
+}, 500);
 
 
 const Latenz = require('latenz');
