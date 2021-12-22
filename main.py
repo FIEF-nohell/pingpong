db9211aae33edea3391cfab6734a748c3c44c3de
FIEF-Nohell <noel.hmnn@gmail.com>
2021-12-22 12:22:53 +0100

    kinda done, no style

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 import threading
 import math
 import os
 
+style.use("fivethirtyeight")
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
 lines = ""
-dataY = ["0"]
-dataX = ["0"]
+dataY = [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
+dataX = [30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 def checkLine(string):
     if(string.find("time=") != -1):
@@ -14,17 +21,24 @@ def checkLine(string):
         return False
 
 def addDummyNumberToArray():
-    dataX.append(0)
-    dataY.append(dataY[len(dataY)-1])
+    num = dataY[len(dataY)-1]
+    dataY.pop(0)
+    dataY.append(num)
+    with open('ping.txt', 'w') as f:
+        for item in dataY:
+            f.write(str(item) + "\n")
 
 def addNumberToArray(string):
-    dataX.append(0)
     time_spot = string.find("time=") + 5
     string = string[time_spot:]
     ms_spot = string.find("ms")
     string = string[:ms_spot]
-    number = math.floor(float(string))
-    dataY.append(number)
+    num = math.floor(float(string))
+    dataY.pop(0)
+    dataY.append(num)
+    with open('ping.txt', 'w') as f:
+        for item in dataY:
+            f.write(str(item) + "\n")
 
 def getData():
   threading.Timer(1, getData).start()
@@ -43,9 +57,17 @@ def getData():
 
 def printData():
   threading.Timer(1.1, printData).start()
-  print("dataX: " + str(dataX))
   print("dataY: " + str(dataY))
   
 
 getData()
 printData()
+
+def update(i):
+    ax1.axis('auto')
+    ax1.invert_xaxis()
+    ax1.clear()
+    ax1.plot(dataX,dataY)
+
+ani = animation.FuncAnimation(fig, update, interval=1000)
+plt.show()
